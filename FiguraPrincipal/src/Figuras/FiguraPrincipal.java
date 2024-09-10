efee685ef26bcae131e009890091ad4b9708d5f7
juancoficial7 <160972348+juancoficial7@users.noreply.github.com>
2024-09-10 12:25:14 -0500

    actividad 5 resuelta

---
+++ FiguraPrincipal/src/Figuras/FiguraPrincipal.java
@@ -0,0 +1,16 @@
+
+package Figuras;
+
+
+public class FiguraPrincipal {
+
+    
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal;
+        miVentanaPrincipal= new VentanaPrincipal();
+        miVentanaPrincipal.setVisible(true); 
+        miVentanaPrincipal.setResizable(false);
+    
+    }
+    
+}
